7e9fd498746882815ec478512e8b61f65881690c
Serhii Bunechko <serhiibunechko@MacBook-Pro-Serhii.local>
2021-04-23 10:09:52 +0300

    Initial commit

---
+++ hw1.py
@@ -0,0 +1,12 @@
+a = int(input(': '))
+b = int(input(': '))
+print((a-b), (a+b), (a*b), (a/b))
+
+name = input('What is your name?\n ')
+print("Hello,", name)
+age = input('How old are you?\n ')
+print('Your age is', age)
+city = input('Where do you live?\n ')
+print('You live in', city)
+
+print('  _________\n /         \\\n |  /\\ /\\  |\n |    -    |\n |  \\___/  |\n \\_________/')
